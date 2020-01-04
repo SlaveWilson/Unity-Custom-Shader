ebde64a9ba9b84a025ee4c3974dc4f4580798817
TNTommy <tommynhm@gmail.com>
2020-01-04 22:39:17 +0800

    Init Commit

---
+++ Assets/Scripts/HologramGlitcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class HologramGlitcher : MonoBehaviour
+{
+    public float glitchChance = .1f;
+
+    private Renderer _renderer;
+    private WaitForSeconds _glitchLoopWait = new WaitForSeconds(.1f);
+    private WaitForSeconds _glitchDuration = new WaitForSeconds(.1f);
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            float glitchTest = Random.Range(0f, 1f);
+
+            if (glitchTest <= glitchChance)
+            {
+                StartCoroutine(Glitch());
+            }
+            yield return _glitchLoopWait;
+        }
+    }
+
+    private IEnumerator Glitch()
+    {
+        _glitchDuration = new WaitForSeconds(Random.Range(.05f, .25f));
+        _renderer.material.SetFloat("_Amount", 1f);
+        _renderer.material.SetFloat("_CutoutThresh", .29f);
+        _renderer.material.SetFloat("_Amplitude", Random.Range(20, 50));
+        _renderer.material.SetFloat("_Speed", Random.Range(1, 10));
+        yield return _glitchDuration;
+        _renderer.material.SetFloat("_Amount", 0f);
+        _renderer.material.SetFloat("_CutoutThresh", 0f);
+    }
+}
---
+++ Assets/Shaders/Outline.shader
@@ -0,0 +1,83 @@
+﻿Shader "Unlit/Outline"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		_Color ("Main Color", Color) = (0,0,0,1)
+		_OutlineColor("Outline Color", Color) = (0,0,0,1)
+		_OutlineWidth("Outline Width", Range(1.0,5.0)) = 1.0
+    }
+
+	CGINCLUDE
+	#include "UnityCG.cginc"
+
+	struct appdata
+	{
+		float4 vertex : POSITION;
+		float3 normal : NORMAL;
+	};
+
+	struct v2f 
+	{
+		float4 pos: POSITION;
+		float3 normal: NORMAL;
+	};
+
+	float4 _OutlineColor;
+	float _OutlineWidth;
+
+	v2f vert(appdata v)
+	{
+		v.vertex.xyz *= _OutlineWidth;
+
+		v2f o;
+		o.pos = UnityObjectToClipPos(v.vertex);
+		return o;
+	}
+
+	ENDCG
+
+    SubShader
+    {
+		Tags{"Queue" = "Transparent"}
+
+       Pass // Render the Outline
+	   {
+			ZWrite Off
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+
+			half4 frag(v2f i) : COLOR 
+			{
+				return _OutlineColor;
+			}
+
+			ENDCG
+	   }
+
+	   Pass // Normal Render
+	   {
+			ZWrite On
+
+			Material
+			{
+				Diffuse[_Color]
+				Ambient[_Color]
+			}
+
+			Lighting On
+
+			SetTexture[_MainTex]
+			{
+				ConstantColor[_Color]
+			}
+
+			SetTexture[_MainTex]
+			{
+				Combine previous * primary DOUBLE
+			}
+	   }
+    }
+}
